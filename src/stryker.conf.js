6ee67baef5e3f0f6003bf42b5e0d25ec65d1e053
Jean-Baptiste LOUVET-DANIEL <jlouvet@hipay.com>
2023-07-27 09:29:21 +0200

    EC-3295 - Fixed stryker report path

--- src/stryker.conf.js
+++ src/stryker.conf.js
@@ -3,6 +3,7 @@ module.exports = {
     ignorePatterns: ['test/integration/**/*.test.js', '.stryker-tmp', 'coverage'],
     packageManager: 'yarn',
     reporters: ['html', 'clear-text', 'progress'],
+    htmlReporter: { fileName: 'reports/mutation/html/index.html' },
     testRunner: 'jest',
     coverageAnalysis: 'off',
     concurrency: 4,
