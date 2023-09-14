9c7ec857ca42daca6f6e002f37b714236539f314
Adam BRECHET <abrechet@hipay.com>
2023-09-14 10:37:41 +0200

    Fix payment card token mapper

--- src/Gateway/Response/Mapper/PaymentMethodMapper.js
+++ src/Gateway/Response/Mapper/PaymentMethodMapper.js
@@ -4,8 +4,8 @@ const AbstractMapper = require('./AbstractMapper');
 const PaymentMethod = require('../PaymentMethod');
 
 class PaymentMethodMapper extends AbstractMapper {
-    mapModel() {
-        return {
+    mapResponseToModel() {
+        const values = {
             token: typeof this.source.token !== 'undefined' ? this.source.token : null,
             brand: typeof this.source.brand !== 'undefined' ? this.source.brand : null,
             pan: typeof this.source.pan !== 'undefined' ? this.source.pan : null,
@@ -16,10 +16,8 @@ class PaymentMethodMapper extends AbstractMapper {
             issuer: typeof this.source.issuer !== 'undefined' ? this.source.issuer : null,
             country: typeof this.source.country !== 'undefined' ? this.source.country : null
         };
-    }
 
-    mapResponseToModel() {
-        this._modelObject = new PaymentMethod(this.mapModel());
+        this._modelObject = new PaymentMethod(values);
     }
 }
 
