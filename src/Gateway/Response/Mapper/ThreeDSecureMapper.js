2f2bbf888597c0227206b0121b354c9b1646137b
Maxime QUERO <mquero.ext@gmail.com>
2024-07-16 14:20:11 +0200

    feat(consultation-api): Handling requestTransactionInformation with consultatin API

--- src/Gateway/Response/Mapper/ThreeDSecureMapper.js
+++ src/Gateway/Response/Mapper/ThreeDSecureMapper.js
@@ -12,7 +12,11 @@ class ThreeDSecureMapper extends AbstractMapper {
             authenticationStatus: typeof this.source.authenticationStatus !== 'undefined' ? this.source.authenticationStatus : null,
             authenticationMessage: typeof this.source.authenticationMessage !== 'undefined' ? this.source.authenticationMessage : null,
             authenticationToken: typeof this.source.authenticationToken !== 'undefined' ? this.source.authenticationToken : null,
-            xid: typeof this.source.xid !== 'undefined' ? this.source.xid : null
+            xid: typeof this.source.xid !== 'undefined' ? this.source.xid : null,
+            scaPreference: typeof this.source.scaPreference !== 'undefined' ? this.source.scaPreference : null,
+            version: typeof this.source.version !== 'undefined' ? this.source.version : null,
+            authenticationMethod: typeof this.source.authenticationMethod !== 'undefined' ? this.source.authenticationMethod : null,
+            liability: typeof this.source.liability !== 'undefined' ? this.source.liability : null,
         };
 
         this._modelObject = new ThreeDSecure(values);
